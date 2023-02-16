8d5262b0e184e237ddb11aff0bfe10e29f8f80c8
gpessoni <pessonigs@gmail.com>
2023-02-16 13:02:10 -0300

    Update the index.js page

--- Aula_01/index.js
+++ Aula_01/index.js
@@ -2,16 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.listen(8080);
+app.listen(8080, () => {
+  console.log("O servidor está rodando na porta 8080");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get('/teste', () => {
-    console.log('Nosso servidor está ativo na porta 8080');
-})
+app.get("/teste", () => {
+  console.log("Nosso servidor está ativo na porta 8080");
+});
+
+let nome = "Gabriel Pessoni";
 
 app.get("/testeComHTML", (req, res) => {
-    res.send("<h1>Server ativo</h1>");
-  });
+  res.sendFile(__dirname + "/index.html");
+});
